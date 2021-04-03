2af0acf2c3e4881c2faff6d50c9465a8674a48f0
Jader Daniel Atehortúa Zapata <daniel0606zapata@gmail.com>
2021-04-03 18:09:54 -0500

    Registro

---
+++ stayawake-nuxt/pages/register.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+      <h1>Registro</h1>
+
+    <template>
+    <v-form ref="formRegister" v-model="valid" lazy-validation>
+
+        <v-text-field 
+            v-model="product.id"
+            :rules="rules.required"
+            label="Identificación" 
+         ></v-text-field>
+
+        <v-text-field 
+            v-model="product.name"
+            :rules="rules.required"
+            label="Nombre"
+             required
+         ></v-text-field>
+
+        <v-text-field 
+            v-model="product.lastName"
+            :rules="rules.required"
+            label="Apellidos" 
+            required
+        ></v-text-field>  
+
+        <v-text-field 
+            v-model="product.mail"
+            :rules="rules.required"
+            label="E-mail" 
+            required
+        ></v-text-field>
+
+        <v-text-field 
+            v-model="product.password"
+            :rules="rules.required"
+            label="Contraseña" 
+            required
+        ></v-text-field>
+
+        <v-select
+            v-model="product.role"
+            :rules="rules.required"
+            :item="role"
+            label="rol"
+            required
+        ></v-select>
+
+        <v-btn color="success" @click="saveProduct()">Guardar producto</v-btn>
+
+    </v-form>
+    </template>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      valid: true,
+      product:{
+        id: "",
+        name: ""
+      },
+      role:["Administrador", "Conductor"], 
+      rules:{
+          required: [v => !!v || "El campo es obligatorio"]
+      }
+    }),
+
+    methods: {
+      saveProduct(){
+          if(this.$refs.formRegister.validate()){
+            let product = Object.assign({}, this.product); 
+            console.log(product);
+          }else{
+            console.log("Formualario incompleto");
+          } 
+      },
+      validate () {
+        this.$refs.form.validate()
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+      resetValidation () {
+        this.$refs.form.resetValidation()
+      },
+    },
+  }
+</script>
+
+<style>
+
+</style>
